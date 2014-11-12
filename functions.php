bba1d13b640d25d26d2c166828fced303a80d691
Bradford Knowlton <brad@ucanbonline.net>
2014-11-12 13:46:13 -0700

    Added image size filters

--- functions.php
+++ functions.php
@@ -10,3 +10,19 @@ add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
 function enqueue_child_theme_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
 }
+
+// et_pb_index_blog_image_width
+
+add_filter('et_pb_index_blog_image_width', 'gd_img_width_filter',10);
+
+function gd_img_width_filter(){
+	return 700;
+}
+
+// et_pb_index_blog_image_height
+
+add_filter('et_pb_index_blog_image_height', 'gd_img_height_filter',10,3);
+
+function gd_img_width_filter(){
+	return 483;
+}
