0334a5adde67584185458e55b205a49f28d3c1d0
Bradford Knowlton <brad@ucanbonline.net>
2014-11-19 19:00:17 -0700

    updated theme from server

--- functions.php
+++ functions.php
@@ -10,3 +10,19 @@ add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
 function enqueue_child_theme_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
 }
+
+// et_pb_portfolio_single_image_width
+
+add_filter('et_pb_portfolio_single_image_width', 'gd_img_width_filter',10, 1);
+
+function gd_img_width_filter($width){
+	return 700;
+}
+
+// et_pb_portfolio_single_image_height
+
+add_filter('et_pb_portfolio_single_image_height', 'gd_img_height_filter',10, 1);
+
+function gd_img_height_filter($height){
+	return 9999;
+}
